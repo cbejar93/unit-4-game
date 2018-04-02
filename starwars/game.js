4e1e5d7905a3a8cd90d8b202052695e08cc0e624
unknown <cbejar93@gmail.com>
2018-04-01 21:47:30 -0400

    I overcomplicate something

--- starwars/game.js
+++ starwars/game.js
@@ -17,7 +17,7 @@ var mace = {health: 97,
 // these varaiables are for the 2 objects fighting. 
 
  var idtoCharacter = {"mace": mace, "luke": luke, "jarjar": jarjar, "c3po":c3po}
-
+// I don't remember what this is for, I just like to have a lot of vars just in case
 var userChoice = {
     health: "",
     attack: "",
@@ -29,12 +29,23 @@ var userFoe ={
     attack:"",
 
 }
-$(document).ready(function(){
-      $(".row").hover(function(){
-      $(".row").css("background-color", "#66afff");
-    });
-});
+var attackagain = true;
+var button = false;
 
+// This prints all the object values on screen
+const printToScreen = () => {
+    
+    $("#lhealth").text(luke.health);
+    $("#lattack").text(luke.attack);
+    $("#jhealth").text(jarJar.health);
+    $("#jattack").text(jarJar.attack);
+    $("#chealth").text(c3po.health);
+    $("#cattack").text(c3po.attack);
+    $("#mhealth").text(mace.health);
+    $("#mattack").text(mace.attack);
+}
+
+printToScreen ();
 
 // this moves the users character into the userSelect area, and removes it from the selector box
 $(".image1").on("click", function(e){
@@ -42,18 +53,9 @@ $(".image1").on("click", function(e){
     var currentTarget = e.currentTarget.id;
     $("#userSelect").append(this);
     var $remaining = $('.cards');
-    console.log($remaining);
+    
     $('#foe').append($remaining);
     
-  
-
-
-    
-   
-
-
-
-
 });
 // this allows the user to choose which enemy they want to go up against first 
 $("#foe").on("click", function(){
@@ -63,21 +65,7 @@ $("#foe").on("click", function(){
     // this creates the attack button, and also the function for how attacking is giong to go
     $("#battle").append("<button>")
         .text("ATTACK")
-        // .click(function (){
-            // this is supposed to target the two images on the screen, the problem is I'm trying to just target the elements id so I can do if/else 
-            // statements and match the picture up with the objects 
-            // for example the picture is luke so i want to get the id luke so i can get the object attributes.e
-            // var userID = $(".image1:nth-child(1)")[0].id;
-            // var enemyID = $(".image1:nth-child(2)")[0].id;
-            // var user = idtoCharacter[userID];
-            // var enemy = idtoCharacter[enemyID];
-            // console.log(user);
-            
-        // })
       
-    
-            
-
 });
 
 $("#battle").on("click", function(){
@@ -85,19 +73,125 @@ $("#battle").on("click", function(){
             var userID = $(".image1:nth-child(1)")[0].id;
             var enemyID = $(".image1:nth-child(2)")[0].id;
             // With the ID in had we match it with their respective object by matching the strings.
-            var user = idtoCharacter[userID];
-            var enemy = idtoCharacter[enemyID];
-            console.log(user);
-            // From the objects respective object values we get the correct health and attack stats of the characters. 
-            attackerhealth = user.health;
-            attackerattack= user.attack;
+            // var attacker = idtoCharacter[userID];
+            // var defender = idtoCharacter[enemyID];
+            // // this is the dynamic variables that should change to represent the attack   
+            // var dcurrenthealth;
+            // var acurrenthealth;
+            // var button = false 
+            
+            // // From the objects respective object values we get the correct and starting health and attack stats of the characters. 
+            // var attackerhealth = attacker.health;
+            // var attackerattack= attacker.attack;
 
-            enemyhealth = enemy.health;
-            enemyattack = enemy.attack;
-            console.log(enemyhealth);
+            // var enemyhealth = defender.health;
+            // var enemyattack = defender.attack;
+            // console.log(enemyhealth);
+
+            const fights = () => {
+                // Luke fight series 
+                if (userID=="luke" && enemyID=="c3po"){
+                        luke.health-=c3po.attack;
+                        c3po.health-=luke.attack
+                        printToScreen ();
+                }
+                if (userID=="luke" && enemyID=="jarjar"){
+                    luke.health-=jarJar.attack;
+                    jarJar.health-=luke.attack
+                    printToScreen ();
+                }
+                if (userID=="luke" && enemyID=="mace"){
+                luke.health-=mace.attack;
+                mace.health-=luke.attack
+                printToScreen ();
+                }
+            
+            // C3PO fight series of functions 
+            if (userID=="c3po" && enemyID=="luke"){
+                c3po.health-=luke.attack;
+                luke.health-=c3po.attack
+                printToScreen ();
+            }
+            if (userID=="c3po" && enemyID=="jarJar"){
+                c3po.health-=jarJar.attack;
+                c3po.health-=c3po.attack
+                printToScreen ();
+            }
+            if (userID=="c3po" && enemyID=="mace"){
+                c3po.health-=mace.attack;
+                mace.health-=c3po.attack
+                printToScreen ();
+            }
+
+            // JARJARS fight series 
+            if (userID=="jarJar" && enemyID=="luke"){
+                jarJar.health-=luke.attack;
+                luke.health-=jarJar.attack
+                printToScreen ();
+            }
+            if (userID=="jarJar" && enemyID=="c3po"){
+                jarJar.health-=c3po.attack;
+                c3po.health-=jarJar.attack
+                printToScreen ();
+            }
+            if (userID=="jarJar" && enemyID=="mace"){
+                jarJar.health-=mace.attack;
+                mace.health-=jarJar.attack
+                printToScreen ();
+            }
+
+            // Mace fight series
+            if (userID=="mace" && enemyID=="luke"){
+                jarJar.health-=luke.attack;
+                luke.health-=jarJar.attack
+                printToScreen ();
+            }
+
+            }
+        fights ();
+
+    console.log(userID);          
+    console.log(enemyID);  
+
+        
+            // if(button=false){
+            //     dcurrenthealth= enemyhealth-= attackerhealth;
+            //     acurrenthealth=attackerhealth-=enemyattack;
+            //     button = true;
+            //     attackagain=false;
+            //     return;
+            // }else if (button=true){
+            //     if (dcurrenthealth > 0 && acurrenthealth > 0){
+            //             dcurrenthealth -= attackerattack;
+            //             attackagain=false;
+            //             return;
+            //     }
+            // }
+            
+            
+            
+        
+            
+            // var button = true;
+            // function attack1(attacker, defender){
+                
+            
+            //     var dcurrenthealth;
+            //     var acurrenthealth;
+            //     if (button){
+            //         dcurrenthealth = defender.health - attacker.attack;
+            //         acurrenthealth = attacker.health - defender.attack;
+            //         test= false;
+            //         return dcurrenthealth;
+            //         return acurrenthealth;
+            //     }else{
+            //         dcurrenthealth-attacker.attack;
+            //         acurrenthealth-defender.health;
+            //     }
+            //      console.log(dcurrenthealth);
+            // }
+            
+  
 });
 
 
-// set variables
-
-// get the users picks 
